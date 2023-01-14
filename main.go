9815d2eb1c0190dbd5aa5943b02b27687fb3afc3
CMES Software <cmessotftware@gmail.com>
2023-01-13 23:23:39 -0300

    change functions visibility

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 	"os"
 )
 
-func readFileLines(filePath string) []string {
+//Keep in mind that the visibility in go package is defined in the object name itself
+//If we want define exported object (public keyword in other languaje such as Python, Java or C#)
+//we must use Capital letter.
+
+func ReadFileLines(filePath string) []string {
 
 	// read the whole content of file and pass it to file variable, in case of error pass it to err variable
 	file, err := os.Open(filePath)
